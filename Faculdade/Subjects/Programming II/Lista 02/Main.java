88efa6e518bfd36e14f2b1bcfda8c7597b58c5b3
Julio C. Oliveira <julio3.oliveir@gmail.com>
2025-01-09 15:26:43 -0300

    Update Main.java

--- Faculdade/Subjects/Programming II/Lista 02/Main.java
+++ Faculdade/Subjects/Programming II/Lista 02/Main.java
@@ -5,27 +5,30 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         // Primeira questão:
+        System.out.println("Primeira Questão:");
         int dia = Utilitarios.validarSeAEntradaEUmInt(scanner, "Insira o número de um dia da semana: ");
         System.out.println(Classificador.diaDaSemana(dia));
 
         // Segunda questão:
+        System.out.println("\nSegunda Questão:");
         System.out.print("Insira um mês: ");
         String mes = scanner.nextLine();
         System.out.println(Classificador.deQueSemestre(mes));
-        System.out.println();
 
         // Terceira questão:
+        System.out.println("\nTerceira Questão:");
         double primeiroValor = Utilitarios.validarSeAEntradaEUmDouble(scanner, "Insira o primeiro valor: ");
         double segundoValor = Utilitarios.validarSeAEntradaEUmDouble(scanner, "Insira o segundo valor: ");
         char operador = Utilitarios.validarSeAEntradaEUmaOperacaoBasica(scanner, "Insira o operador: ");
         System.out.println(Matematica.calcularOperacoesBasicas(primeiroValor, segundoValor, operador));
-        System.out.println();
 
         // Quarta questão:
-        int numeroDeTermos = Utilitarios.validarSeAEntradaEUmInt(scanner, "Insira o numero de termos á serem calculados: ");
+        System.out.println("\nQuarta Questão:");
+        int numeroDeTermos = Utilitarios.validarSeAEntradaEUmInt(scanner, "Insira o numero de termos à serem calculados: ");
         System.out.println(Matematica.calcularConstante(numeroDeTermos));
 
         // Quinta questão:
+        System.out.println("\nQuinta Questão:");
         Biblioteca biblioteca = new Biblioteca();
 
         biblioteca.adicionarLivro("God of War", "Kratos", 2005);
