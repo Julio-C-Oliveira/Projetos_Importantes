b4bb1bfab0f09157c3fee605e6491913b7e98acf
Júlio C Oliveira <julio3.oliveir@gmail.com>
2025-02-14 12:04:55 -0300

    Criando a classe Game Essa classe será responsável por: - Armazenar: 	- Estado do jogo. 	- Número de hérois. 	- Número de vilões. 	- Players com os menores atributos. 	- Players com os maiores atributos. 	- Armazenar os Logs. - Controlar o fluxo do jogo. - Gerenciar os turnos. - Oferecer diferentes níveis de dificuldade.

---
+++ gameSection/Game.java
@@ -0,0 +1,14 @@
+package gameSection;
+
+public class Game {
+    // gameState, o que exatamente eu tenho que guardar aqui?
+    int numberOfHeros;
+    int numberOfVillains;
+    // Players com os maiores atributos.
+    // Players com os menores atributos.
+    // Logs
+
+    // Métodos para Iniciar e Finalizar o jogo
+    // Gerencia de Turnos
+    // Diferentes Níveis de Dificuldade
+}
