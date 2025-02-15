58b27cb14725bc97811150bfdc3b184cdc8f022a
Savio-Miranda <saviomiranda.dev@gmail.com>
2025-02-15 14:37:24 -0300

    unificação de battlelog e attributelog

--- testSection/TestesParaPokemon.java
+++ testSection/TestesParaPokemon.java
@@ -1,57 +1,7 @@
-// public class testBattle {
-//     static Log log = new Log();
-//     static Turn turn = new Turn();
-
-//     public static void testar() {
-//         Pokemon friend = Pokemon.takeAPokemon(
-//                 "01",
-//                 (short) 47,
-//                 Type.NORMAL,
-//                 Type.FIRE);
-//         Pokemon foe = Pokemon.takeAPokemon(
-//                 "02",
-//                 (short) 127,
-//                 Type.WATER,
-//                 Type.FIRE);
-        
-//         String event;
-//         int priority = turn.getNextPokemon(friend, foe);
-//         if (priority == 1){
-//             event = turn.makeAction(foe, friend);
-//             log.appendLog(event);
-//         }
-
-//         for (int i = 0; i < 30; i++) {
-//             if (foe.getHealthPoints() <= 0){
-//                 log.appendLog("O pokémon vencedor é: " + friend.getPokemonName());
-//                 break;
-//             }
-            
-//             if (friend.getHealthPoints() <= 0){
-//                 log.appendLog("O pokémon vencedor é: " + foe.getPokemonName());
-//                 break;
-//             }
-
-//             if (i%2 == priority){
-//                 event = turn.makeAction(friend, foe);
-//             } else {
-//                 event = turn.makeAction(foe, friend);
-//             }
-//             log.appendLog(event);
-            
-//         }
-//     }
-//     public static void main(String[] args) {
-//         testar();
-//         log.showLog();
-//     }
-// }
-
 package testSection;
 
-import logSection.AttributeLog;
 import turnSection.Turn;
-import logSection.BattleLog;
+import logSection.Log;
 import pokemonSection.pokedex.PokeRocket;
 import pokemonSection.pokedex.PokeSOX;
 import pokemonSection.pokedex.Pokemon;
@@ -60,8 +10,7 @@ import pokemonSection.constants.Type;
 
 public class TestesParaPokemon {
     public static void testar() {
-        BattleLog battleLog = new BattleLog(); // Instanciando um log para gravar a batalha.
-        AttributeLog attributeLog = new AttributeLog(); // Instanciando um log para gravar atributos por turno.
+        Log log = new Log(); // Instanciando um log para gravar batalhas e atributos por turno
         Turn turn = new Turn(); // Intanciando um turno para gerenciar a batalha.
         /* Settar o Nível dos Vilões e dos Heróis, isso é uma constante.
         *  Heróis deixei settado em 5.
@@ -89,27 +38,28 @@ public class TestesParaPokemon {
                 Type.WATER,
                 Type.FIRE);
         
-        battleLog.recordBattleInstant(friend, foe);
-        attributeLog.recordCurrentBestAttributes(turn.getCurrentTurn());
+        log.recordBattleInstant(friend, foe);
+        log.recordCurrentBestAttributes(turn.getCurrentTurn());
         
         String battleEvent;
         int priority = turn.getNextPokemon(friend, foe);
         if (priority == 1){
             battleEvent = turn.makeAction(foe, friend);
-            battleLog.appendLog(battleEvent);
-            attributeLog.recordCurrentBestAttributes(turn.getCurrentTurn());
+            log.appendLog(battleEvent);
+            log.recordCurrentBestAttributes(turn.getCurrentTurn());
         }
 
 
         for (int i = 0; i < 100; i++) {
             if (turn.verifyIfDead(friend)){
                 battleEvent = "O Vencedor é o Pokémon:\n" + foe.getPokemonName() + "\nO Perdedor é o Pokémon:\n" + friend.getPokemonName();
-                battleLog.appendLog(battleEvent);
+                log.appendLog(battleEvent);
                 break;
             }
+
             if (turn.verifyIfDead(foe)){
                 battleEvent = "O Vencedor é o Pokémon:\n" + friend.getPokemonName() + "\nO Perdedor é o Pokémon:\n" + foe.getPokemonName();
-                battleLog.appendLog(battleEvent);
+                log.appendLog(battleEvent);
                 break;
             }
 
@@ -118,12 +68,12 @@ public class TestesParaPokemon {
             } else {
                 battleEvent = turn.makeAction(foe, friend);
             }
-            battleLog.appendLog(battleEvent);
-            attributeLog.recordCurrentBestAttributes(turn.getCurrentTurn());
+
+            log.appendLog(battleEvent);
+            log.recordCurrentBestAttributes(turn.getCurrentTurn());
         }
 
-        attributeLog.showLog();
-        battleLog.showLog();
+        log.showLog();
     }
 
     public static void main(String[] args) {
