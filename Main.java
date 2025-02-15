00880e60d9f7e3d2d32798feeb89b34691701d99
Julio C. Oliveira <julio3.oliveir@gmail.com>
2025-02-15 15:34:39 -0300

    Tirei uma linha em branco da Main

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 import testSection.TestesParaPokemon;
 
-
 public class Main {
     public static void main(String[] args) {
         TestesParaPokemon.testar(); // Fiz uma função para testar as funções das classes relacionadas aos Pokémons para não poluir a main.
